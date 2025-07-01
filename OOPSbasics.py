cf73a9e99204ee1ef4e9ef602832ff5bad07ca62
santhoshrajkovuru2025 <srkovuru@gmail.com>
2025-07-01 14:01:41 +0530

    Added the Constructor and Inheritance Concepts

--- OOPSbasics.py
+++ OOPSbasics.py
@@ -1,16 +1,32 @@
+##Notes:
+
+# 'Self' keyword is mandatory for calling the variable names into the method
+# Instance and class variables have whole different purpose
+# Constructor name should be __init__
+# 'new' keyword is not required for creation of object.
+
+
 class Calculator:
     num=100
 
     # default Constructor
-    def __init__(self):
+    def __init__(self,a,b):
+        self.firstNumber=a
+        self.secondNumber=b
         print('I am a default constructor')
 
     def getData(self):
         print('I am a Method')
 
-obj = Calculator() # syntax to create the objects in python
+    def Summation(self):
+        return self.firstNumber+self.secondNumber+Calculator.num
+
+
+obj = Calculator(5,6) # syntax to create the objects in python
 obj.getData()
-print(obj.num)
+print(obj.Summation())
 
 
-
+obj = Calculator(7,9) # syntax to create the objects in python
+obj.getData()
+print(obj.Summation())
