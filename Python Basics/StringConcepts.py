ae9c092ce5d090dc5a38139d30d1d6a6339fed42
santhoshrajkovuru2025 <srkovuru@gmail.com>
2025-07-02 13:50:07 +0530

    Added the folder

---
+++ Python Basics/StringConcepts.py
@@ -0,0 +1,26 @@
+str = "SanthoshRaj.website"
+str1 = "Kovuru"
+str2 = "Raj"
+str3 ="padma"
+
+
+print(str[1]) # To print the string as like array
+
+print(str[0:5]) # if you want to print the sub string
+
+print(str+str1) # concatination
+
+print(str2 in str) #substring check
+
+print(str3 in str2)
+
+var = str.split(".")
+print(var)
+print(var[0])# To get the index of the variable
+
+str4 ="keerthana"
+print(str4.strip())
+
+print(str4.lstrip())
+print(str4.rstrip())
+
