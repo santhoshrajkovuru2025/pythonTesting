2ecc82b20d0074ce96774575f793211b490746b2
santhoshrajkovuru2025 <srkovuru@gmail.com>
2025-07-10 15:34:10 +0530

    New file names - LIST is added

---
+++ Python Basics/LIST.py
@@ -0,0 +1,10 @@
+nums = [12,24,36,48,60,"SanthoshRaj",9.6,True]
+names= ["Santhosh Raj","Prashanth",9.6,"Akash",'A','y']
+Misc =[nums, names]
+print(nums)
+print(nums[1])
+print(nums[3:])
+print(nums[-1])
+print(nums[-5:])
+print(Misc)
+
