befe4b86ecfaf5e72dbd632e9816ae077dbd4447
santhoshrajkovuru2025 <srkovuru@gmail.com>
2025-07-24 17:05:50 +0530

    Added string programs

---
+++ Practice/Numeric characters.py
@@ -0,0 +1,20 @@
+class numeric_files:
+
+    a=89
+    print(a)
+    print(type(a))
+
+    b=12.9
+    print(b)
+    print(type(b))
+
+    c=12-7J
+    print(c)
+    print(type(c))
+
+    d = 125.274
+    print(d)
+    print(type(d))
+
+    
+
---
+++ Python Basics/StringType.py
@@ -0,0 +1,12 @@
+s='you are awesome'
+print(s)
+s1="""You're the one
+      product of the
+      creator of the universe"""
+print(s1)
+print(s1[0])
+print(len(s1))
+print(s[:8])
+print(s1[0:9:2]) #string slicing
+print(s1[0:15:3])
+print(s1[20::-3])
