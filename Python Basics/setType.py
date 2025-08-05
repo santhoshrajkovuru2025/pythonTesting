3b27f29824ce0a260af41b9a67b164cc4ca41da8
santhoshrajkovuru2025 <srkovuru@gmail.com>
2025-08-05 06:35:49 -0400

    Added few lines of code in List, Tuple and create a new program on Set

---
+++ Python Basics/setType.py
@@ -0,0 +1,11 @@
+# set:
+# '{}' is used to create a set type.
+# set does not support indexing
+# set does not support the duplication of values in printing the values
+
+s ={1,2,3,4,5,6,7,8,9,10,10,9,8}
+print(s)
+print(type(s))  # used to print the type of the list
+s.update([11,12,13,14]) # is used to update the values
+print(s)
+
--- Python Basics/tupleType.py
+++ Python Basics/tupleType.py
@@ -9,5 +9,21 @@
 
 tpl = (1.0,2.0,11,22,33,"strange",'K')
 
-print(tpl)
+print(tpl) # prints the tuple set
+print(tpl*3) # prints the tuple list 3 times
+print(type(tpl)) # prints the type of set
+print(len(tpl)) # prints the length of the tuple
+print(tpl[3]) # prints the given index
+print(tpl.count(20))
+print(tpl.index(33))
+
+
+# list can be converted to the tuple.
+
+lst = [1,2,3,4,5,6,7,8,9,10]
+print(type(lst))
+tp=tuple(lst)
+print(tp)
+print(type(tp))
+
 
