9337718eaf41b18b0d14b2a5d5224133cf811140
santhoshrajkovuru2025 <srkovuru@gmail.com>
2025-08-06 17:42:10 +0530

    Message Transfer

---
+++ Practice/list.py
@@ -0,0 +1,18 @@
+list1=["India","USA","UK"]
+
+print(list1)
+print(type(list1))
+list1.append("Germany")
+print(list1)
+del(list1[2])
+print(list1)
+list1.insert(1,"England")
+print(list1)
+
+set1 ={"Indonesia","Australia","New Zealand"}
+print(set1)
+set1.update(["Maldives"])
+print(set1)
+set1.remove("Australia")
+print(set1)
+
--- Python Basics/setType.py
+++ Python Basics/setType.py
@@ -1,11 +1,17 @@
 # set:
 # '{}' is used to create a set type.
-# set does not support indexing
+# set does not support indexing and slicing
 # set does not support the duplication of values in printing the values
+# we have 'frozenset' that means we cant perform any operation on frozen set.
+# frozenset doesn't support the update and remove attributes
+
 
 s ={1,2,3,4,5,6,7,8,9,10,10,9,8}
 print(s)
 print(type(s))  # used to print the type of the list
 s.update([11,12,13,14]) # is used to update the values
 print(s)
+s.remove(10)
+
+f = frozenset(s)
 
